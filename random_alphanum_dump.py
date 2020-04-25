d043e202319f1958608f70c7c96a01ed99d30740
Aaron J. Olson <aaolson817@gmail.com>
2020-04-25 15:44:21 -0500

    update alphanum dump

--- random_alphanum_dump.py
+++ random_alphanum_dump.py
@@ -1,12 +1,14 @@
 from uuid import uuid4
 from time import sleep
+import random
 '''
 Generates a stream of random alphanumeric characters
 '''
 
 for i in range(10000):
-    sleep(.07)
+    sleep(.07)  # slows output down a bit, crude way to regulate the output speed
     output = ''
-    for s in range(10):
+    for s in range(random.randint(4, 17)):  # arbitrary range
         output += str(uuid4()).replace('-', '')
-    print(output)
+
+    print(output)  # makes newlines
